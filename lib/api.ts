b8a3f9eb627ecb8b6e3828551c13ccd3f1e3d67e
cchoirun <niamchoirun020@gmail.com>
2026-08-02 22:47:12 +0700

    change host to deploy

--- lib/api.ts
+++ lib/api.ts
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api',
 });
 
 api.interceptors.request.use((config) => {
